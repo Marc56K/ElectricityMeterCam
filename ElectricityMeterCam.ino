86d4cc8f7fc7755cd647227c7ba2c76e31574358
Marc Roßbach <>
2020-11-14 16:25:05 +0100

    refactor: remove some code

--- ElectricityMeterCam.ino
+++ ElectricityMeterCam.ino
@@ -1,118 +1,203 @@
 #include "esp_camera.h"
 #include <WiFi.h>
-#include "soc/soc.h" //disable brownout problems
-#include "soc/rtc_cntl_reg.h"  //disable brownout problems
-
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
-
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_ESP_EYE
-//#define CAMERA_MODEL_M5STACK_PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE
-#define CAMERA_MODEL_AI_THINKER
-
+#include "soc/soc.h"  //disable brownout problems
+#include "soc/rtc_cntl_reg.h"   //disable brownout problems
+#include "esp_http_server.h"
 #include "camera_pins.h"
 #include "wifi_config.h"
+#define PART_BOUNDARY "123456789000000000000987654321"
+static
+const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary="
+PART_BOUNDARY;
+static
+const char *_STREAM_BOUNDARY = "\r\n--"
+PART_BOUNDARY "\r\n";
+static
+const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
-void startCameraServer();
+httpd_handle_t stream_httpd = NULL;
 
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
-  
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
+static esp_err_t stream_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t *_jpg_buf = NULL;
+    char *part_buf[64];
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if (res != ESP_OK)
+    {
+        return res;
+    }
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
+    while (true)
+    {
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            Serial.println("Camera capture failed");
+            res = ESP_FAIL;
+        }
+        else
+        {
+            if (fb->width > 400)
+            {
+                if (fb->format != PIXFORMAT_JPEG)
+                {
+                    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                    if (!jpeg_converted)
+                    {
+                        Serial.println("JPEG compression failed");
+                        res = ESP_FAIL;
+                    }
+                }
+                else
+                {
+                    _jpg_buf_len = fb->len;
+                    _jpg_buf = fb->buf;
+                }
+            }
+        }
 
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  while (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    Serial.println();
-    delay(100);
-  }
+        if (res == ESP_OK)
+        {
+            size_t hlen = snprintf((char*) part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            res = httpd_resp_send_chunk(req, (const char *) part_buf, hlen);
+        }
 
-  sensor_t * s = esp_camera_sensor_get();
-  //initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);//flip it back
-    s->set_brightness(s, 1);//up the blightness just a bit
-    s->set_saturation(s, -2);//lower the saturation
-  }
-  //drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, (const char *) _jpg_buf, _jpg_buf_len);
+        }
 
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
 
-  WiFi.begin(WIFI_SSID, WIFI_KEY);
+        if (fb)
+        {
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        }
+        else if (_jpg_buf)
+        {
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+        if (res != ESP_OK)
+        {
+            break;
+        }
 
-  startCameraServer();
+        Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+    }
 
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+    return res;
 }
 
-void loop() 
+void startCameraServer()
 {
-  /*
-  camera_fb_t *pic = esp_camera_fb_get();
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.server_port = 80;
 
-  if (pic != nullptr)
-    Serial.printf("Picture taken! Its size was: %d bytes", pic->len);
-  else
-    Serial.print("esp_camera_fb_get failed!");
-  Serial.println();
-*/
-  delay(1000);
+    httpd_uri_t index_uri = { .uri = "/",
+        .method = HTTP_GET,
+        .handler = stream_handler,
+        .user_ctx = NULL
+    };
+
+    //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+    if (httpd_start(&stream_httpd, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(stream_httpd, &index_uri);
+    }
+}
+
+void setup()
+{
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  //disable brownout detector
+
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println();
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG; //PIXFORMAT_GRAYSCALE;
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    while (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        Serial.println();
+        delay(100);
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_vflip(s, 1); //flip it back
+        s->set_brightness(s, 1);  //up the blightness just a bit
+        s->set_saturation(s, -2); //lower the saturation
+    }
+
+    WiFi.begin(WIFI_SSID, WIFI_KEY);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("http://");
+    Serial.println(WiFi.localIP());
+
+    startCameraServer();
+}
+
+void loop()
+{
+    /*
+    camera_fb_t *pic = esp_camera_fb_get();
+
+    if (pic != nullptr)
+      Serial.printf("Picture taken! width: %d height: %d size: %d bytes", pic->width, pic->height, pic->len);
+    else
+      Serial.print("esp_camera_fb_get failed!");
+    Serial.println();
+
+    delay(1000);
+    */
+    delay(1);
 }
