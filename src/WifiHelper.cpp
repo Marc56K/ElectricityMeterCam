b08b4f56e14d89b6cd40065c10ea833d9fad39a8
Marc Roßbach <>
2020-11-28 16:57:56 +0100

    feat: set default hostname to 'esp32cam'

--- src/WifiHelper.cpp
+++ src/WifiHelper.cpp
@@ -2,10 +2,18 @@
 #include <Arduino.h>
 #include "wifi_config.h"
 
+#ifndef HOSTNAME
+#define HOSTNAME "esp32cam"
+#endif
+
 void WifiHelper::Connect()
 {
     Serial.print("Connecting to WiFi: ");
     Serial.println(WIFI_SSID);
+
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
+    WiFi.setHostname(HOSTNAME);
+    
     WiFi.begin(WIFI_SSID, WIFI_KEY);
     delay(500);
     if (!WiFi.isConnected())
@@ -17,11 +25,10 @@ void WifiHelper::Connect()
         while (!WiFi.isConnected())
         {
             delay(1000);
-            Serial.print("retry.");
+            Serial.println("retry.");
         }
     }
 
-    Serial.println("");
     Serial.println("WiFi connected");
     Serial.print("http://");
     Serial.println(WiFi.localIP());
