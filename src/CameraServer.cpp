9dbd749cec47dd0607f59a11e85c98e14028082f
Marc Roßbach <>
2020-12-07 22:12:16 +0100

    fix: reduce xclk_freq_hz to fix sporatic camera errors

--- src/CameraServer.cpp
+++ src/CameraServer.cpp
@@ -182,7 +182,7 @@ bool CameraServer::InitCamera(const bool flipImage)
     config.pin_sscb_scl = SIOC_GPIO_NUM;
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
+    config.xclk_freq_hz = 10000000;
     config.pixel_format = PIXFORMAT_JPEG;
     config.frame_size = FRAMESIZE_QVGA;
     config.jpeg_quality = 12;
